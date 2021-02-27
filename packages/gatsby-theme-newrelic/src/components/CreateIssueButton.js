0adf9e10795343450c01d560625b2e93f4563e98
Austin Schaefer <55203603+austin-schaefer@users.noreply.github.com>
2021-02-26 16:52:22 -0800

    fix: Improve issue template w/ language from yes/no buttons

--- packages/gatsby-theme-newrelic/src/components/CreateIssueButton.js
+++ packages/gatsby-theme-newrelic/src/components/CreateIssueButton.js
@@ -7,22 +7,19 @@ import useThemeTranslation from '../hooks/useThemeTranslation';
 import useInstrumentedHandler from '../hooks/useInstrumentedHandler';
 
 const ISSUE_BODY = `
-## Description
-
-[NOTE]: # (Describe the problem you're encountering.)
-[TIP]: # (Do NOT give us access or passwords to your New Relic account or API keys!)
-
-## Steps to Reproduce
-
-[NOTE]: # (Please be as specific as possible.)
-
-## Expected Behavior
-
-[NOTE]: # (Tell us what you expected to happen.)
-
-## Relevant Logs / Console output
-
-[NOTE]: # (Please provide specifics of the local error logs, Browser Dev Tools console, etc. if appropriate and possible.)
+<!-- Thanks for filing an issue on our docs! Your feedback helps us improve our
+docs for every New Relic user. -->
+<!-- **THIS REPO IS PUBLIC. Anything you share here is visible to the world,
+so be careful with screenshots and sensitive data.** -->
+### Tell us what you need
+* If you've found something inaccurate, what is it?
+* If something is hard to read, confusing, or missing information, let us know.
+* If you're having trouble completing a task or are uncertain what to do next, 
+  tell us what you're trying to do so we can provide more useful information.
+### Anything else you'd like to share?
+Add other context like screenshots, links to other docs, and information about
+your environment (operating system, application framework, etc.).
+### Doc information (don't delete this section)
 `;
 
 const CreateIssueButton = ({
